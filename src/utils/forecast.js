ad2f5c34b618ff6835cdd94af61c7ff77c4c73fe
Mukul Tripathi <mukultripathi.2000@gmail.com>
2021-05-08 21:40:53 +0530

    weather data chanegd

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined,' It is currently ' + body.current.temperature + ' degress out and '+body.current.weather_descriptions[0]+'. There is a ' + body.current.precip + 'mm precipitation with '+body.current.humidity+'% humidity!!')
         }
     })
 }
